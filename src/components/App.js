b43402cef4888302007e34a235b291cded29402e
Ana Carvalho <anapaulacmoura@gmail.com>
2022-12-04 17:25:50 -0300

    creating Topo component

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import GlobalStyle from "../css/GlobalStyle";
+import styled from 'styled-components';
+import Container from "./Container";
+
+
+
+
+export default function App() {
+  return (
+    <ScreenContainer>
+      <GlobalStyle />
+      <Container />
+      </ScreenContainer>
+  );
+}
+
+
+const ScreenContainer = styled.div`
+    background-color: #FB6B6B;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    padding-bottom: 200px;
+`
---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+
+import styled from 'styled-components';
+import Topo from './Topo';
+
+export default function Container() {
+    return (
+       <Topo/>
+
+    )
+}
+
+
